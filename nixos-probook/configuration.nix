7ea4d5c02e67c0eb458e327e20322da9be75cfb0
fyredragon69 <awillettand@gmail.com>
2024-04-16 15:23:41 -0400

    add git to system packages

--- nixos-probook/configuration.nix
+++ nixos-probook/configuration.nix
@@ -131,6 +131,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    git
     firefox
     nano
     python3

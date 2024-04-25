55257d6eac64094fc053fbc980c37fa43a6e9747
fyredragon69 <awillettand@gmail.com>
2024-04-24 23:00:26 -0400

    add pv

--- home.nix
+++ home.nix
@@ -29,6 +29,7 @@
     zsh
     git
     wget
+    pv
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

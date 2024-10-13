f8f0df0839046e1ce1bde7f853770d92d71bdc5d
fyredragon69 <awillettand@gmail.com>
2024-10-13 15:27:42 -0400

    nixos-probook/hardware-configuration.nix: proper formatting using nixfmt

--- nixos-probook/hardware-configuration.nix
+++ nixos-probook/hardware-configuration.nix
@@ -4,28 +4,33 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ehci_pci"
+    "ahci"
+    "usb_storage"
+    "sd_mod"
+    "sr_mod"
+    "rtsx_pci_sdmmc"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/df76368a-ea75-43de-8c02-5a1837c87e1c";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/df76368a-ea75-43de-8c02-5a1837c87e1c";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-	{ device = "/dev/sda2";
-	  fsType = "vfat";
-	};
+  fileSystems."/boot" = {
+    device = "/dev/sda2";
+    fsType = "vfat";
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/05bc1174-1f36-4d02-9296-829619a2ba30"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/05bc1174-1f36-4d02-9296-829619a2ba30"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -36,5 +41,6 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

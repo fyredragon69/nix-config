46a4407993d80621c38ef13b06b2c9dce0e50625
fyredragon69 <awillettand@gmail.com>
2024-10-13 15:23:56 -0400

    flake.nix: proper formatting using nixfmt

--- flake.nix
+++ flake.nix
@@ -21,141 +21,141 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, jovian, nixos-unstable, nix-darwin, hax-nur, ... }: let
-    mkSpecialArgs = (me: system: {
-      inherit me;
-      hax-nur = hax-nur.outputs.packages.${system};
-    });
-   in {
-    homeConfigurations.awill = home-manager.lib.homeManagerConfiguration (let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      inherit pkgs;
-
-      # Specify your home configuration modules here, for example,
-      # the path to your home.nix.
-      modules = [ ./home.nix ];
-
-      # Optionally use extraSpecialArgs
-      # to pass through arguments to home.nix
-    }); # homeConfigurations.awill
-    darwinConfigurations = {
-      "Mac-Mini" = nix-darwin.lib.darwinSystem (let
-        me = "awill";
-        system = "aarch64-darwin";
-      in rec {
-          inherit system;
-          
-          specialArgs = mkSpecialArgs me system;
-          modules = [
-          ./cfg-jvms.nix
-          ];
+  outputs =
+    { nixpkgs, home-manager, jovian, nixos-unstable, nix-darwin, hax-nur, ... }:
+    let
+      mkSpecialArgs = (me: system: {
+        inherit me;
+        hax-nur = hax-nur.outputs.packages.${system};
       });
+    in {
+      homeConfigurations.awill = home-manager.lib.homeManagerConfiguration (let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      }); # homeConfigurations.awill
+      darwinConfigurations = {
+        "Mac-Mini" = nix-darwin.lib.darwinSystem (let
+          me = "awill";
+          system = "aarch64-darwin";
+        in rec {
+          inherit system;
+
+          specialArgs = mkSpecialArgs me system;
+          modules = [ ./cfg-jvms.nix ];
+        });
+      };
+
+      nixosConfigurations.nixdeck = nixpkgs.lib.nixosSystem (let
+        system = "x86_64-linux";
+        #pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        inherit system;
+
+        modules = [
+          ./nixos-nixdeck/hardware-configuration.nix
+          ./nixos-nixdeck/configuration.nix
+          jovian.nixosModules.jovian
+          {
+            jovian = {
+              steam = {
+                enable = true;
+                autoStart = true;
+                #desktopSession can be plasma or plasmawayland.
+                desktopSession = "plasma";
+                user = "deck";
+              };
+              devices.steamdeck = {
+                enable = true;
+                autoUpdate = true;
+                enableGyroDsuService = true;
+                enablePerfControlUdevRules = true;
+                enableSoundSupport = true;
+                enableControllerUdevRules = true;
+                enableXorgRotation = true; # should play with this later...
+              };
+              decky-loader = { enable = false; };
+            };
+          }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.deck = { pkgs, ... }: {
+                imports = [ ./home.nix ];
+
+                home = {
+                  username = pkgs.lib.mkForce "deck";
+                  homeDirectory = pkgs.lib.mkForce /home/deck;
+                };
+                programs.home-manager.enable = pkgs.lib.mkForce false;
+              };
+            };
+          }
+        ];
+      }); # nixosConfigurations.nixdeck
+      nixosConfigurations.Probook-650 = nixpkgs.lib.nixosSystem (let
+        system = "x86_64-linux";
+        #pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        inherit system;
+
+        modules = [
+          ./nixos-probook/hardware-configuration.nix
+          ./nixos-probook/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.awill = { pkgs, ... }: {
+                imports = [ ./home.nix ];
+
+                home = {
+                  username = pkgs.lib.mkForce "awill";
+                  homeDirectory = pkgs.lib.mkForce /home/awill;
+                };
+                programs.home-manager.enable = pkgs.lib.mkForce false;
+              };
+            };
+          }
+        ];
+      }); # nixosConfigurations.Framework
+      nixosConfigurations.Framework = nixpkgs.lib.nixosSystem (let
+        system = "x86_64-linux";
+        #pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        inherit system;
+
+        modules = [
+          ./nixos-framework/hardware-configuration.nix
+          ./nixos-framework/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.awill = { pkgs, ... }: {
+                imports = [ ./home.nix ];
+
+                home = {
+                  username = pkgs.lib.mkForce "awill";
+                  homeDirectory = pkgs.lib.mkForce /home/awill;
+                };
+                programs.home-manager.enable = pkgs.lib.mkForce false;
+              };
+            };
+          }
+        ];
+      }); # nixosConfigurations.Framework
     };
-    
-    nixosConfigurations.nixdeck = nixpkgs.lib.nixosSystem (let
-      system = "x86_64-linux";
-      #pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      inherit system;
-
-      modules = [
-        ./nixos-nixdeck/hardware-configuration.nix
-        ./nixos-nixdeck/configuration.nix
-        jovian.nixosModules.jovian
-        {
-          jovian = {
-            steam = {
-              enable = true;
-              autoStart = true;
-              #desktopSession can be plasma or plasmawayland.
-              desktopSession = "plasma";
-              user = "deck";
-            };
-            devices.steamdeck = {
-              enable = true;
-              autoUpdate = true;
-              enableGyroDsuService = true;
-              enablePerfControlUdevRules = true;
-              enableSoundSupport = true;
-              enableControllerUdevRules = true;
-              enableXorgRotation = true; # should play with this later...
-            };
-            decky-loader = { enable = false; };
-          };
-        }
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useUserPackages = true;
-            useGlobalPkgs = true;
-            users.deck = { pkgs, ... }: {
-              imports = [ ./home.nix ];
-
-              home = {
-                username = pkgs.lib.mkForce "deck";
-                homeDirectory = pkgs.lib.mkForce /home/deck;
-              };
-              programs.home-manager.enable = pkgs.lib.mkForce false;
-            };
-          };
-        }
-      ];
-    }); # nixosConfigurations.nixdeck
-    nixosConfigurations.Probook-650 = nixpkgs.lib.nixosSystem (let
-      system = "x86_64-linux";
-      #pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      inherit system;
-
-      modules = [
-        ./nixos-probook/hardware-configuration.nix
-        ./nixos-probook/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useUserPackages = true;
-            useGlobalPkgs = true;
-            users.awill = { pkgs, ... }: {
-              imports = [ ./home.nix ];
-
-              home = {
-                username = pkgs.lib.mkForce "awill";
-                homeDirectory = pkgs.lib.mkForce /home/awill;
-              };
-              programs.home-manager.enable = pkgs.lib.mkForce false;
-            };
-          };
-        }
-      ];
-    }); # nixosConfigurations.Framework
-    nixosConfigurations.Framework = nixpkgs.lib.nixosSystem (let
-      system = "x86_64-linux";
-      #pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      inherit system;
-
-      modules = [
-        ./nixos-framework/hardware-configuration.nix
-        ./nixos-framework/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useUserPackages = true;
-            useGlobalPkgs = true;
-            users.awill = { pkgs, ... }: {
-              imports = [ ./home.nix ];
-
-              home = {
-                username = pkgs.lib.mkForce "awill";
-                homeDirectory = pkgs.lib.mkForce /home/awill;
-              };
-              programs.home-manager.enable = pkgs.lib.mkForce false;
-            };
-          };
-        }
-      ];
-    }); # nixosConfigurations.Framework
-  };
 }

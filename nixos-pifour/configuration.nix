ed1dd5bdb4b6436a3ec1dfd8183a6964b8992c6c
fyredragon69 <awillettand@gmail.com>
2024-12-12 17:45:10 -0500

    nixos-pifour: comment out bootloader related options for now

--- nixos-pifour/configuration.nix
+++ nixos-pifour/configuration.nix
@@ -45,9 +45,9 @@
   services.tailscale.enable = true;
 
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
+  # boot.loader.grub.enable = false;
   # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
+  # boot.loader.generic-extlinux-compatible.enable = true;
 
   networking.hostName = "pifour-nixos"; # Define your hostname.
   # Pick only one of the below networking options.

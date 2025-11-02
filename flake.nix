120cc5e2fed5706f5a2e242ea3b8106454b0a7eb
fyredragon69 <awillettand@gmail.com>
2025-11-02 10:24:55 -0500

    flake: fix hm modules path

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ sys-configs/hm-deck/home.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
